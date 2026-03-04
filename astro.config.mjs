b1fb8d9dbdfb68b8c7a75c27f8460e0f7968cf22
shivank kunwar <shivankkunwar100@gmail.com>
2026-03-04 21:27:29 +0530

    pls final

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,15 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  server: {
+    allowedHosts: true
+  },
   vite: {
     ssr: {
       noExternal: ['three']
     },
     optimizeDeps: {
       include: ['three']
-    },
-    preview: {
-      allowedHosts: true
     }
   }
 });
